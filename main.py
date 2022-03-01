71dc78e2050d9d8985c928b24307c79e401ef7a6
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-03-01 14:32:22 +0100

    better debug + fonction parse

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ class Decoupeur:
         self.analysed = self.analyse()
         self.generer()
 
+
     def polissage(self, code) -> str:
-        code = code.replace("\n", " ")
-        code = code.replace("\t", " ")
+        code = code.replace("\n", " ").replace("\t", " ").strip()
         if DEBUG_PRINT:
-            print("Polissage :")
-            print(code)      
+            print(f"Polissage :\n| {code}")
         return code
+
 
     def decoupe(self) -> list:
         analiser_codetemp = lambda code: [c.strip() for c in code.split(",")]
@@ -41,10 +41,11 @@ class Decoupeur:
 
         exit_list.append([oldpush, push, analiser_codetemp(codetemp)])
         if DEBUG_PRINT:
-            print("\nDecoupe :")
+            print("\nDecoupe - [in args, out args, [code]] :")
             for e in exit_list:
-                print(e)
+                print(f"| {e}")
         return exit_list
+
 
     def analyse(self) -> list:
         analysed = []
@@ -52,38 +53,51 @@ class Decoupeur:
             if len(e[2]) != e[1] and self.decouped.index(e) != len(self.decouped) - 1:
                 raise Exception("le nombre de chevron ne correspond pas au nombre de parametres")
             for i in range(len(e[2])):
-                c = str(e[2][i])
-                if c.isdigit():
-                    # si c'est un nombre
-                    analysed.append(["V", f"stream{i}" ,c.strip()])
-                elif c[0] == c[-1] and c[0] in ["'", '"']:
-                    # si c'est une chaine
-                    analysed.append(["V", f"stream{i}" ,c[1:-1]])
-                elif c[0] == "$":
-                    # si c'est une variable
-                    if e[0] > 1:
-                        raise Exception("une variable ne peut pas prendre plusieurs entrées")
-                    if len(analysed) == 0:
-                        analysed.append(["H", f"stream{i}", c[1:]])
-                    else:
-                        analysed.append(["H", c[1:], f"stream{i}"])
-                else:
-                    # si c'est une fonction
-                    analysed.append(["T", c ,"&".join([f"stream{j+i}" for j in range(e[0] // (e[1] if e[1] != 0 else 1))])])
-                    if e[1] > 0:
-                        analysed[-1].append(f"stream{i}")
+                analysed.append(self.parse(e, i, len(analysed)))
+                
         if DEBUG_PRINT:
             print("\nAnalyse :")
             for a in analysed:
-                print(a)
+                print(f"| {a}")
         return analysed
+
+
+    def parse(self, e, i, length):
+        c = str(e[2][i])
+        
+        # si c'est un nombre
+        if c.isdigit():
+            return ["V", f"stream{i}" ,c.strip()]
+        
+        # si c'est une chaine
+        elif c[0] == c[-1] and c[0] in ["'", '"']:
+            return ["V", f"stream{i}" ,c[1:-1]]
+        
+        # si c'est une variable
+        elif c[0] == "$":
+            if e[0] > 1:
+                raise Exception("une variable ne peut pas prendre plusieurs entrées")
+            if length == 0:
+                return ["H", f"stream{i}", c[1:]]
+            else:
+                return ["H", c[1:], f"stream{i}"]
+        
+        # si c'est une fonction
+        else:
+            sortie = ["T", c ,"&".join([f"stream{j+i}" for j in range(e[0] // (e[1] if e[1] != 0 else 1))])]
+            if e[1] > 0:
+                sortie.append(f"stream{i}")
+            while "" in sortie:
+                sortie.remove("")
+            return sortie
+        
 
     def generer(self) -> str:
         print("\nGeneration :")
         for e in self.analysed:
-            print(" ".join(e))
+            print("| " + " ".join(e))
 
 
 Decoupeur("""
-1, 0 >> or > $coucou > print
+coucou > print
 """)
